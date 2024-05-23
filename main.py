9d2caf2974a3f02cadfe0e9f906d1cb33f8bcc86
Alexei Malinovskiy <6328673@gmail.com>
2024-05-23 11:26:04 +0300

    feat: task complete

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from pprint import pprint
+import os.path
 from response_downloader import ResponseDownloader
 
-DOWNLOAD_PATH = 'C:\\Users\\63286\\Pictures\\PDD'
+DOWNLOAD_DIR = os.path.join(os.path.dirname(__file__), 'download')
+FILE_LOG = 'download.log'
 CONTENT_TYPE = 'application/pdf'
-COUNT = 1
-URLS = [
-    'https://xn--90adear.xn--p1ai/upload/site1000/folder/original/avtovladeltsam/fees/abm-2023/',
-    'https://xn--90adear.xn--p1ai/upload/site1000/folder/original/avtovladeltsam/fees/cd-2023/'
+REQUESTS = [
+    {
+        'url': 'https://xn--90adear.xn--p1ai/upload/site1000/folder/original/avtovladeltsam/fees/abm-2023/',
+        'dir': os.path.join(DOWNLOAD_DIR, 'ABM'),
+        'count': 40,
+    },
+    {
+        'url': 'https://xn--90adear.xn--p1ai/upload/site1000/folder/original/avtovladeltsam/fees/cd-2023/',
+        'dir': os.path.join(DOWNLOAD_DIR, 'CD'),
+        'count': 40,
+    },
 ]
 
 if __name__ == '__main__':
-    for url in URLS:
-        print(f'--- GET ЗАПРОСЫ по адресу:  "{url}" ---')
-        downloader = ResponseDownloader(CONTENT_TYPE, url, COUNT, DOWNLOAD_PATH)
-        result = downloader.get_result()
-        pprint(result)
+    for req in REQUESTS:
+        downloader = ResponseDownloader(CONTENT_TYPE, req.get('url'), req.get('count'), req.get('dir'))
+        downloader.get_result()
--- response_downloader.py
+++ response_downloader.py
@@ -1,4 +1,8 @@
+import os
+import sys
+
 import requests
+import logging
 from datetime import datetime
 
 
@@ -10,53 +14,56 @@ class ResponseDownloader:
         self.count = count
         self.download_path = download_path
 
+        self.logger = logging.getLogger(url)
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(logging.INFO)
+        stdout_handler.setFormatter(formatter)
+
+        file_handler = logging.FileHandler(filename=os.path.join(download_path, 'requests.log'),
+                                           encoding='utf-8', mode='w')
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(stdout_handler)
+
     def get_headers(self):
         return {
             'Content-Type': self.content_type
         }
 
-    def get_response(self, filename: str):
-        file_url = f'{self.url}{filename}'
-        headers = self.get_headers()
-
-        response = requests.get(file_url, headers=headers)
-        return response.content
-
-    def save_content(self, content, filename):
-        try:
-            with open(f'{self.download_path}{filename}', 'wb') as f:
-                f.write(content)
-                return 'OK'
-        except Exception as e:
-            return str(e)
-
     def get_file(self, filename):
-        full_filename = f'{self.download_path}{filename}'
-        request = {
-            'datetime_begin': self.now(),
-            'filename': full_filename,
-        }
-        content = self.get_response(f'{filename}.pdf')
-        request['result'] = self.save_content(content, full_filename)
-        request['datetime_end'] = self.now()
+        full_filename = os.path.join(self.download_path, filename)
+        file_url = f'{self.url}{filename}.pdf'
 
-        return request
+        headers = self.get_headers()
+        self.logger.info(f'{filename} GET: headers={headers}, url={file_url}')
+        response = requests.get(file_url, headers=headers)
+
+        if response:
+            try:
+                self.logger.warning(f'ОТВЕТ: status_code={response.status_code}')
+                if response.status_code == 200:
+                    with open(full_filename, 'wb') as f:
+                        f.write(response.content)
+                return response.status_code
+            except Exception as e:
+                self.logger.error(f'ОТВЕТ: {str(e)}')
+                return str(e)
 
     def get_result(self):
-        response = {
-            'url': self.url,
-            'datetime_begin': self.now(),
-            'requests': [],
-        }
+        self.logger.info(f'НАЧАЛО ЗАПРОСОВ ДЛЯ АДРЕСА: {self.url}')
 
         for i in range(0, self.count):
-            filename = f'{i}_1_15.pdf'
-            response['requests'] += self.get_file(filename)
-
-        response['datetime_end'] = self.now()
-
-        return response
+            self.get_file(f'{i+1}_1-5.pdf')
+            self.get_file(f'{i+1}_6-10.pdf')
+            self.get_file(f'{i+1}_11-15.pdf')
+            self.get_file(f'{i+1}_16-20.pdf')
 
     @staticmethod
-    def now():
-        return datetime.now().strftime('%m/%d/%y %H:%M:%S')
+    def now() -> str:
+        result = datetime.now().strftime('%m.%d.%y %H:%M:%S')
+        return result
